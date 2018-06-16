9273f580071cee5b2ba369fae7154204c48155f8
Pogromist <12221569+PogromistDev@users.noreply.github.com>
2018-06-16 21:42:54 +0400

    upload

---
+++ Bmp.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.IO;
+
+namespace BMPVisualizer
+{
+    public class Bmp
+    {
+        public BinaryReader stream;
+        public FileHeader fileHeader;
+        public ImageHeader imageHeader;
+
+        public uint scanLineWidth;
+        public bool scanLineOrder;
+
+        public bool zeroRemainder;
+
+        public uint remainder;
+        public uint pad;
+
+        public Bmp(Stream stream)
+        {
+            this.stream = new BinaryReader(stream);
+        }
+
+        public void ReadFileHeader()
+        {
+            stream.BaseStream.Seek(0, SeekOrigin.Begin);
+
+            fileHeader.signature = stream.ReadChars(2);
+            fileHeader.size = stream.ReadUInt32();
+            fileHeader.reserved1 = stream.ReadUInt16();
+            fileHeader.reserved2 = stream.ReadUInt16();
+            fileHeader.offset = stream.ReadUInt32();
+        }
+
+        public void ReadImageHeader()
+        {
+            stream.BaseStream.Seek(14, SeekOrigin.Begin);
+
+            imageHeader.size = stream.ReadUInt32();
+            imageHeader.width = stream.ReadUInt32();
+            imageHeader.height = stream.ReadInt32();
+
+            imageHeader.planes = stream.ReadInt32();
+            imageHeader.bits = stream.ReadUInt16();
+
+            imageHeader.compression = stream.ReadUInt32();
+            imageHeader.imageSize = stream.ReadUInt32();
+
+            imageHeader.xresPerMeter = stream.ReadUInt32();
+            imageHeader.yresPerMeter = stream.ReadUInt32();
+
+            imageHeader.colorUsed = stream.ReadUInt32();
+            imageHeader.significantColors = stream.ReadUInt32();
+
+            remainder = imageHeader.width * 3 % 4;
+            pad = 4 - remainder;
+
+            zeroRemainder = remainder == 0;
+
+            scanLineWidth = (zeroRemainder ? imageHeader.width * 3 : imageHeader.width * 3 + 4 - remainder);
+            scanLineOrder = imageHeader.height < 0;
+        }
+
+        public byte[] ReadAsByteArray()
+        {
+            SeekToPixelData();
+
+            int size = (int)imageHeader.width * imageHeader.height * 3;
+            byte[] buffer = new byte[size];
+
+            int i = 0;
+
+            for (int y = 0; y < Math.Abs(imageHeader.height); y++)
+            {
+                for (int x = 0; x < imageHeader.width; x++)
+                {
+                    BmpColor color = ReadPixel();
+
+                    buffer[i * 3] = color.r;
+                    buffer[i * 3 + 1] = color.g;
+                    buffer[i * 3 + 2] = color.b;
+
+                    i++;
+                }
+
+                if (!zeroRemainder) stream.BaseStream.Seek(pad, SeekOrigin.Current);
+            }
+
+            return buffer;
+
+        }
+
+        public void SeekToPixelData()
+        {
+            stream.BaseStream.Seek(fileHeader.offset, SeekOrigin.Begin);
+        }
+
+        public BmpColor ReadPixel()
+        {
+            BmpColor color;
+
+            color.b = stream.ReadByte();
+            color.g = stream.ReadByte();
+            color.r = stream.ReadByte();
+
+            return color;
+        }
+
+        public BmpColor[] ReadScanLine()
+        {
+            BmpColor[] colors;
+
+            colors = new BmpColor[imageHeader.width];
+            for (int i = 0; i < colors.Length; i++)
+            {
+                colors[i] = ReadPixel();
+            }
+
+            if (!zeroRemainder)
+            {
+                stream.BaseStream.Seek(pad, SeekOrigin.Current);
+            }
+
+            return colors;
+        }
+
+        public void CloseStream()
+        {
+            stream.Close();
+        }
+
+        //Structs
+
+        public struct FileHeader
+        {
+            public char[] signature;
+            public uint size;
+            public ushort reserved1;
+            public ushort reserved2;
+            public uint offset;
+        }
+
+        public struct ImageHeader
+        {
+            public uint size;
+
+            public uint width;
+            public int height;
+
+            public int planes;
+            public ushort bits;
+
+            public uint compression;
+            public uint imageSize;
+
+            public uint xresPerMeter;
+            public uint yresPerMeter;
+
+            public uint colorUsed;
+            public uint significantColors;
+        }
+
+        public struct BmpColor
+        {
+            public byte r, g, b;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BMPVisualizer
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bit;
+        uint dataOffset;
+        uint width;
+        int height;
+        bool sign = false;
+        ushort bitFormat = 0;
+        ushort planes = 1;
+
+        Color[] pallete;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReadBMP();
+        }
+
+        //BMP functionality
+
+        void ReadBMP(string fileName = "1.bmp")
+        {
+            Random rnd = new Random();
+
+            try
+            {
+                FileStream fs = new FileStream(fileName, FileMode.Open);
+                BinaryReader br = new BinaryReader(fs, Encoding.ASCII);
+
+                //Read
+
+                br.BaseStream.Seek(10, SeekOrigin.Begin);
+
+                dataOffset = br.ReadUInt32();
+
+                br.BaseStream.Seek(18, SeekOrigin.Begin);
+
+                width = br.ReadUInt32();
+                height = br.ReadInt32();
+
+                planes = br.ReadUInt16();
+
+                bitFormat = br.ReadUInt16();
+
+                sign = (height < 0);
+
+                height = Math.Abs(height);
+
+                bit = new Bitmap((int)width * planes, height);
+
+                //Data reading
+
+                Color color;
+
+                if (bitFormat == 24)
+                {
+                    br.BaseStream.Seek(dataOffset, SeekOrigin.Begin);
+
+                    uint remainder = (width * 3) % 4;
+                    bool zeroRemainder = (remainder == 0);
+                    int scanLineOrder;
+
+                    for (int hgh = 0; hgh < height; hgh++)
+                    {
+                        for (int wdth = 0; wdth < width; wdth++)
+                        {
+                            byte R, G, B;
+
+                            B = br.ReadByte();
+                            G = br.ReadByte();
+                            R = br.ReadByte();
+
+                            color = Color.FromArgb(R, G, B);
+
+                            scanLineOrder = (!sign ? height - 1 - hgh : hgh);
+
+                            bit.SetPixel(wdth, scanLineOrder, color);
+                        }
+
+                        if (!zeroRemainder)
+                        {
+                            uint padding = 4 - remainder;
+                            br.BaseStream.Seek(padding, SeekOrigin.Current);
+                        }
+                    }
+                }
+
+                if (bitFormat <= 8)
+                {
+                    br.BaseStream.Seek(dataOffset - (dataOffset - 54), SeekOrigin.Begin);
+
+                    pallete = new Color[(dataOffset - 54) / 4];
+                    for (int i = 0; i < pallete.Length; i++)
+                    {
+                        byte r, g, b;
+                        b = br.ReadByte();
+                        g = br.ReadByte();
+                        r = br.ReadByte();
+                        br.ReadByte();
+
+                        pallete[i] = Color.FromArgb(r, g, b);
+                    }
+
+                    br.BaseStream.Seek(dataOffset, SeekOrigin.Begin);
+
+                    uint byteCount = (uint)Math.Ceiling((float)width / 8);
+                    uint remainder = (byteCount % 4);
+                    uint padding = 4 - remainder;
+                    bool zeroRemainder = (remainder == 0);
+                    int scanLineOrder;
+
+                    for (int hgh = 0; hgh < height; hgh++)
+                    {
+                        byte[] colors = br.ReadBytes((int)byteCount);
+
+                        int wdth = 0;
+
+                        for (int xx = 0; xx < width; xx++)
+                        {
+                            wdth = xx / 8;
+
+                            int mask = (1 << bitFormat) - 1;
+
+                            //byte rgb = (byte)( (colors[wdth] & (1 << 7 >> (xx % 8))) >> (7 - (xx % 8)) );
+                            byte rgb = ReadBits(colors[wdth], (byte)bitFormat, (byte)(xx % 8));
+
+                            //byte rgb = (byte)((colors[wdth] & ( (mask << (8 - bitFormat)) >> (xx % 8))) >> (7 - (xx % 8)) );
+
+                            color = pallete[rgb];
+
+                            scanLineOrder = (!sign ? height - 1 - hgh : hgh);
+
+                            bit.SetPixel(xx, scanLineOrder, color);
+                        }
+                        
+
+                        if (!zeroRemainder)
+                        {
+                            br.BaseStream.Seek(padding, SeekOrigin.Current);
+                        }
+                    }
+                }
+
+                //Close file
+
+                br.Close();
+                fs.Close();
+            }
+            catch (System.IO.IOException e)
+            {
+                MessageBox.Show("File is in use by: " + e.Source);
+            }
+        }
+
+        void ReadGarbageAsBMP(string fileName = "garbage.bmp")
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open);
+
+            BinaryReader br = new BinaryReader(fs, Encoding.ASCII);
+
+            //Read
+
+            long fileSize = br.BaseStream.Seek(0, SeekOrigin.End);
+
+            br.BaseStream.Seek(0, SeekOrigin.Begin);
+
+            width = (uint)widthNum.Value;
+            height = (int)heightNum.Value;
+
+            bitFormat = 24;
+
+            sign = (height < 0);
+
+            height = Math.Abs(height);
+
+            bit = new Bitmap((int)width, height);
+
+            //Data reading
+
+            Color color;
+
+            int scanLineOrder;
+
+            for (int hgh = 0; hgh < height; hgh++)
+            {
+                for (int wdth = 0; wdth < width; wdth++)
+                {
+                    byte R, G, B;
+
+                    if (br.BaseStream.Position < fileSize - 3)
+                    {
+                        R = br.ReadByte();
+                        G = br.ReadByte();
+                        B = br.ReadByte();
+
+                        color = Color.FromArgb(R, G, B);
+
+                        scanLineOrder = (!sign ? (height - 1) - hgh : hgh);
+
+                        bit.SetPixel(wdth, scanLineOrder, color);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            //Close file
+
+            br.Close();
+            fs.Close();
+
+            Invalidate();
+        }
+
+        byte ReadBits(byte value, byte count, byte offset)
+        {
+            byte mask = (byte)((Math.Pow(2, count) - 1));
+            mask <<= (8 - (count + offset));
+
+            return (byte)((value & (mask)) >> (8 - count - offset));
+        }
+
+        //
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (bit != null)
+            {
+                e.Graphics.TranslateTransform(0, 38);
+                e.Graphics.DrawImage(bit, 0, 0);
+            }
+        }
+
+        //Drag and Drop
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            ReadBMP(((string[])e.Data.GetData(DataFormats.FileDrop))[0]);
+            Invalidate();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            DoDragDrop(bit, DragDropEffects.Copy);
+        }
+
+        //Garbage
+
+        private void readGarbageButton_Click(object sender, EventArgs e)
+        {
+            ReadGarbageAsBMP(garbageFileName.Text);
+        }
+
+        private void saveGarbageAsImageButton_Click(object sender, EventArgs e)
+        {
+            bit.Save("image.png");
+        }
+    }
+}
